3bef7c88edfb53068fc587f53813b08b2ab264c0
Raul-Docs <raul.cal.bar@gmail.com>
2022-05-16 13:03:00 +0200

    Añadido aside-left e imagen movil con menor tamaño

---
+++ javascript/sidebar.js
@@ -0,0 +1,16 @@
+function showDiv1() {
+  document.getElementById("div1").style.display = "block"
+  document.getElementById("div2").style.display = "none"
+  document.getElementById("div3").style.display = "none"
+}
+
+function showDiv2() {
+  document.getElementById("div1").style.display = "none"
+  document.getElementById("div2").style.display = "block"
+  document.getElementById("div3").style.display = "none"
+}
+function showDiv3() {
+  document.getElementById("div1").style.display = "none"
+  document.getElementById("div2").style.display = "none"
+  document.getElementById("div3").style.display = "block"
+}
